4dc470dcf2fc5aff7a1fd90244b91e64cf198502
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-15 15:30:36 +0530

    Gravity.java: print output

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -22,6 +22,7 @@ public class Gravity {
         time = scanner.nextDouble();
         speed = accOfGravity * time;
         distance = (accOfGravity * time * time) / 2;
+        System.out.println("The speed of the object at " + time + " seconds after its release is " + speed + " and the distance the object has travelled in the " + time + " seconds after the relase is " + distance);
 
 
 
