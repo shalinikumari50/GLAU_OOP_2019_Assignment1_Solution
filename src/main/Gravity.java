8042ec369a5342bb4160ca709b17b210e87da18a
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-15 15:25:33 +0530

    Gravity.java: add variables

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -4,7 +4,6 @@
  * */
 package main;
 
-// import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
 public class Gravity {
@@ -15,6 +14,12 @@ public class Gravity {
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
 		 *   c. Calculate the result and print it to the screen.
 		 * */
+        double accOfGravity = 9.8;
+        double time;
+        double speed;
+        double distance;
+
+
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
